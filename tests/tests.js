c8fe4d5d4218b7758a65379f0a1c73396460778b
davidmartins <davids.martinz@gmail.com>
2016-04-03 13:11:55 +0100

    First commit with the basic Health Tracker Structure

--- tests/tests.js
+++ tests/tests.js
@@ -9,9 +9,31 @@ describe('Health Tracker Test Framework', function () {
 
     it('Should have mocha installed and running.', function () {
         assert.equal(true, true);
-    });
+    })
 
     it('Should have should library installed for fluent testing.', function () {
         true.should.eql(true);
+    })
+})
+
+describe('Asynchronous testing.', function () {
+
+    var somethingHere = 2;
+    var somethingElse = 'check';
+
+    var result = false;
+
+    beforeEach(function (done) {
+        setTimeout(function () {
+            result = true;
+            // complete the async beforeEach
+            done();
+        }, 2000);
     });
-});
+
+    it('Should NOT be so hard so this makes it easier', function (done) {
+        result.should.eql(true);
+        done();
+    });
+
+})
